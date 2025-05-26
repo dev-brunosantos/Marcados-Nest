f8afa88672079e9adf151d116251d37aa24e2d06
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-26 14:16:41 -0300

    Iniciando o desenvolvimento das funcionalidades da categoria CARGOS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CargosModule } from './cargos/cargos.module';
 
 @Module({
-  imports: [],
+  imports: [CargosModule],
   controllers: [AppController],
   providers: [AppService],
 })
