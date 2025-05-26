730df2aa579625199b5897af2572e14598700185
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-26 17:27:50 -0300

    Iniciando a criação das funcionalidades da categoria de USUARIOS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CargosModule } from './cargos/cargos.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [CargosModule],
+  imports: [CargosModule, UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
