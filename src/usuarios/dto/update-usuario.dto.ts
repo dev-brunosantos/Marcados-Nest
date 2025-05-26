324834221ba2748db57b80d82422ecf4b454e3a8
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-26 17:56:17 -0300

    Criando o modelo de DTO de atualização de dados do usuario

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+    nome: string;
+    senha: string;
+    cargo: string;
+    naipe: string;
+}
