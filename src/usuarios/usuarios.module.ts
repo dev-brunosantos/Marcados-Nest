e0985b3c8b851098d7067e358580028fe7aaae3b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-26 18:59:59 -0300

    Importando o modulo de CARGOS

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CargosModule } from 'src/cargos/cargos.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CargosModule],
   controllers: [UsuariosController],
   providers: [UsuariosService],
 })
