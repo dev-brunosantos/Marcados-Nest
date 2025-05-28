340df85db5515edd2a975e8e08b350e41aaddd6b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-28 00:54:59 -0300

    Importando o modulo de VOZ

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CargosModule } from './cargos/cargos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { VozModule } from './voz/voz.module';
 
 @Module({
-  imports: [CargosModule, UsuariosModule],
+  imports: [CargosModule, UsuariosModule, VozModule],
   controllers: [AppController],
   providers: [AppService],
 })
