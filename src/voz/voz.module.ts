f0360ede4e19d7471bea03d3391bf92445955169
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-28 00:56:14 -0300

    Importando o modulo do prisma e exportando as funcionalidades do service de VOZ

--- src/voz/voz.module.ts
+++ src/voz/voz.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VozService } from './voz.service';
 import { VozController } from './voz.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [VozController],
   providers: [VozService],
+  exports: [VozService]
 })
 export class VozModule {}
