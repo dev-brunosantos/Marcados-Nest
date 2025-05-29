3075965383bc5ec963ff12cfb8a77b5152a6cbb6
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-29 18:30:03 -0300

    Importando o prisma e exportando o Modulo para uso em outros trechos do sistema

--- src/naipe/naipe.module.ts
+++ src/naipe/naipe.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NaipeService } from './naipe.service';
 import { NaipeController } from './naipe.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NaipeController],
   providers: [NaipeService],
+  exports: [NaipeService]
 })
 export class NaipeModule {}
