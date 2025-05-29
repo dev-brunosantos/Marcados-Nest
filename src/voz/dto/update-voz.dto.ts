26c5d45423cb634ab45e3dd4bcc89d8bd39485b8
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-29 10:06:04 -0300

    Removendo codigo desnecessário

--- src/voz/dto/update-voz.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVozDto } from './create-voz.dto';
-
-export class UpdateVozDto extends PartialType(CreateVozDto) {}
--- src/voz/entities/voz.entity.ts
+++
@@ -1,6 +0,0 @@
-export class Voz {
-    id: number;
-    naipe: string;
-    dtCriacao: Date;
-    dtAtualizacao: Date;
-}
