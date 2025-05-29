ac93cc02d1b7407da6264599295e91e9c24be1d4
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-29 19:19:24 -0300

    Adicionando novos dados testes no banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CargosModule } from './cargos/cargos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { VozModule } from './voz/voz.module';
 import { NaipeModule } from './naipe/naipe.module';
 
 @Module({
-  imports: [CargosModule, UsuariosModule, VozModule, NaipeModule],
+  imports: [CargosModule, UsuariosModule, NaipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
