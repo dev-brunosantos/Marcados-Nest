9f9f3fd5a3af7faea820f5b73a8d507f8f0bd1d9
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-29 18:34:32 -0300

    Criando os metodos privados da classe

--- src/naipe/naipe.service.ts
+++ src/naipe/naipe.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNaipeDto } from './dto/create-naipe.dto';
 import { UpdateNaipeDto } from './dto/update-naipe.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class NaipeService {
+
+  constructor(private prisma: PrismaService) {}
+
+  private async naipeExistente(nome: string) {
+    const naipe = await this.prisma.naipe.findFirst({
+      where:{ naipe: nome }
+    })
+
+    return naipe
+  }
+  
+  private async naipeId(id: number) {
+    const naipeId = await this.prisma.naipe.findFirst({
+      where: { id }
+    })
+
+    return naipeId
+  } 
+
   create(createNaipeDto: CreateNaipeDto) {
     return 'This action adds a new naipe';
   }
