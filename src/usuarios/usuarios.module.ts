d7bfe905d8828ee9b2daaa03db27266f6f7d8739
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-05-30 15:32:58 -0300

    Importando as funcionalidades do modulo de naipes

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -3,10 +3,11 @@ import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CargosModule } from 'src/cargos/cargos.module';
+import { NaipeModule } from 'src/naipe/naipe.module';
 
 @Module({
-  imports: [PrismaModule, CargosModule],
+  imports: [PrismaModule, CargosModule, NaipeModule],
   controllers: [UsuariosController],
   providers: [UsuariosService],
 })
-export class UsuariosModule {}
+export class UsuariosModule { }
